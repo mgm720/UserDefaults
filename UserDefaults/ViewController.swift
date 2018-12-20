182aad174e21dc64ab13f7e0166dd6d6fa41e731
Michael Miles <michael.gage.miles@gmail.com>
2018-12-20 13:23:40 -0800

    Added mp3 file and created sound capability.

--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    let defaults = UserDefaults.standard
+    
+    var playMusic = true
+    
+    var player: AVAudioPlayer?
+    
+    @IBOutlet weak var musicButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if playMusic == true {
+            playSong()
+        } else if playMusic == false {
+            return
+        }
     }
 
 
+    func playSong() {
+        let soundURL = Bundle.main.url(forResource: "bensound-ukulele", withExtension: "mp3")
+        
+        do {
+            try player = AVAudioPlayer(contentsOf: soundURL!)
+        } catch {
+            print(error)
+        }
+        
+        player?.play()
+    }
 }
 
